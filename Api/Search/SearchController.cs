5d8f2a88060744e0c0b8725799ed96bf55cf596b
koskokos <kostya.shnypir@gmail.com>
2017-11-14 16:01:50 +0200

    stub for search action

--- Api/Search/SearchController.cs
+++ Api/Search/SearchController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Npgsql;
-using NpgsqlTypes;
 using Search.PostgresRDKit.Tables;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Search
 {
@@ -16,33 +13,57 @@ namespace Search
         //const string connectionString = "User ID=postgres;Host=rdkit-postgres;Port=5432;Database=simsearch;Pooling=true;";
         const string connectionString = "User ID=postgres;Host=192.168.99.100;Port=5432;Database=simsearch;Pooling=true;";
 
+        readonly static string searchCommand = $"SELECT " +
+            $"{nameof(molecules_raw.idnumber)}, " +
+            $"{nameof(molecules_raw.smiles)}, " +
+            $"{nameof(molecules_raw.name)}, " +
+            $"{nameof(molecules_raw.mw)}, " +
+            $"{nameof(molecules_raw.logp)}, " +
+            $"{nameof(molecules_raw.hba)}, " +
+            $"{nameof(molecules_raw.hbd)}, " +
+            $"{nameof(molecules_raw.rotb)}, " +
+            $"{nameof(molecules_raw.tpsa)}, " +
+            $"{nameof(molecules_raw.fsp3)}, " +
+            $"{nameof(molecules_raw.hac)} " +
+            $"FROM {nameof(molecules_raw)} " +
+            $"LIMIT @Limit";
+
+
         [HttpPost]
         [Route("search")]
         public SearchResult Search(string smiles, string searchType, int hitLimit, int pageSize)
         {
-            throw new NotImplementedException();
-            //try
-            //{
-            //    using (var con = new NpgsqlConnection(connectionString))
-            //    {
-            //        var command = con.CreateCommand();
-            //        command.CommandText = "SELECT datname FROM pg_database WHERE datistemplate = false;";
-            //        //    command.CommandText = "select id, mol from mols where tanimoto_sml(rdkit_fp('CN(CC1=CSC=N1)S(C)(=O)=O'::mol), fp) > 0.7;";
-            //        con.Open();
-            //        var reader = command.ExecuteReader();
-            //        return reader.GetTextReader(0).ReadToEnd();
+            return new SearchResult { Molecules = SearchCore(hitLimit) };
+        }
 
-            //        //    while (reader.Read())
-            //        //    {
-            //        //        var id = reader["id"];
-            //        //        var smiles = reader["mol"];
-            //        //    }
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    return e.ToString();
-            //}
+        private IEnumerable<MoleculeData> SearchCore(int hitLimit)
+        {
+            using (var con = new NpgsqlConnection(connectionString))
+            {
+                var command = con.CreateCommand();
+                command.CommandText = searchCommand;
+                command.Parameters.Add(new NpgsqlParameter("@Limit", hitLimit));
+
+                con.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    yield return new MoleculeData
+                    {
+                        Idnumber = (string)reader[nameof(molecules_raw.idnumber)],
+                        Smiles = (string)reader[nameof(molecules_raw.smiles)],
+                        Name = (string)reader[nameof(molecules_raw.name)],
+                        Mw = (double)reader[nameof(molecules_raw.mw)],
+                        Logp = (double)reader[nameof(molecules_raw.logp)],
+                        Hba = (int)reader[nameof(molecules_raw.hba)],
+                        Hbd = (int)reader[nameof(molecules_raw.hbd)],
+                        Rotb = (int)reader[nameof(molecules_raw.rotb)],
+                        Tpsa = (double)reader[nameof(molecules_raw.tpsa)],
+                        Fsp3 = (double)reader[nameof(molecules_raw.fsp3)],
+                        Hac = (int)reader[nameof(molecules_raw.hac)],
+                    };
+                }
+            }
         }
 
         readonly static string oneCommand = $"SELECT " +
