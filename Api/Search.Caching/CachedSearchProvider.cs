21f0300d416157d6528fd255e2614e583676a6f6
koskokos <kostya.shnypir@gmail.com>
2017-12-01 15:17:01 +0200

    little changes

---
+++ Api/Search.Caching/CachedSearchProvider.cs
@@ -0,0 +1,21 @@
+﻿using Search.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace Search.Caching
+{
+    public class CachedSearchProvider<TId> : ISearchProvider<TId>
+    {
+        readonly ISearchProvider<TId> _search;
+
+        public CachedSearchProvider(ISearchProvider<TId> underlyingProvider)
+        {
+            _search = underlyingProvider;
+        }
+
+        public Task<ISearchResult<TId>> FindAsync(SearchQuery searchQuery, int fastFetchCount)
+        {
+            
+        }
+    }
+}
