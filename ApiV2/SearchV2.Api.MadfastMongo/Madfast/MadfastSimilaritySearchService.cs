a9902605919ca49ddc4c303ddcba99623a00cafd
koskokos <kostya.shnypir@gmail.com>
2017-12-20 14:48:00 +0200

    refactored out AsyncResult from MFSS

--- ApiV2/SearchV2.Api.MadfastMongo/Madfast/MadfastSimilaritySearchService.cs
+++ ApiV2/SearchV2.Api.MadfastMongo/Madfast/MadfastSimilaritySearchService.cs
@@ -21,211 +21,47 @@ namespace SearchV2.Api.MadfastMongo
             _hitLimit = hitLimit;
         }
 
+        class MadfastQueryResult
+        {
+            public IEnumerable<Target> Targets { get; set; }
+
+            public class Target
+            {
+                public double Dissimilarity { get; set; }
+                public string Targetid { get; set; }
+            }
+        }
+
         Task<ISearchResult<MadfastResultItem>> ISearchService<string, MadfastSearchQuery, MadfastResultItem>.FindAsync(MadfastSearchQuery query, int fastFetchCount)
-        {
-            return Task.FromResult<ISearchResult<MadfastResultItem>>(new Result(this, fastFetchCount, _hitLimit, 1.0 - query.SimilarityThreshold, query.Query));
-        }
-
-        class Result : ISearchResult<MadfastResultItem>, IAsyncOperation
-        {
-            volatile Task _runningTask;
-            readonly List<MadfastResultItem> loaded = new List<MadfastResultItem>();
-            readonly object _syncObj = new object();
-
-            async Task<IEnumerable<MadfastResultItem>> Query(string url, string query, double maxDissimilarity, int count)
-            {
-                var r = await _httpClient.PostAsync(url, new FormUrlEncodedContent(new Dictionary<string, string>
-                    {
-                        { "max-count", count.ToString() },
-                        { "query", query },
-                        { "max-dissimilarity", maxDissimilarity.ToString() }
-                    }));
-
-                return r.IsSuccessStatusCode
-                    ? JsonConvert
-                        .DeserializeObject<MadfastQueryResult>(await r.Content.ReadAsStringAsync())
-                        .Targets
-                        .Select(t => new MadfastResultItem { Ref = t.Targetid, Similarity = 1.0 - t.Dissimilarity })
-                        .ToList()
-                    : Enumerable.Empty<MadfastResultItem>();
-            }
-
-            public Result(MadfastSimilaritySearchService creator, int fastFetch, int hitLimit, double maxDissimilarity, string query)
-            {
-                _runningTask = Task.Run(async () =>
-                {
-                    var res = await Query(creator._url, query, maxDissimilarity, fastFetch);
-
-                    lock (_syncObj)
-                    {
-                        loaded.AddRange(res);
-                        _runningTask = res.Count() < hitLimit
-                            ? Task.Run(async () =>
+        => Task.FromResult<ISearchResult<MadfastResultItem>>(
+            new AsyncResult<MadfastResultItem>(
+                updateState => 
+                    Task.Run(() =>
+                        {
+                            async Task LoadAndUpdate(int count, Func<int, Task> nextFactory)
                             {
-                                var res1 = await Query(creator._url, query, maxDissimilarity, hitLimit);
-                                lock (_syncObj)
+                                var r = await _httpClient.PostAsync(_url, new FormUrlEncodedContent(new Dictionary<string, string>
                                 {
-                                    loaded.Clear();
-                                    loaded.AddRange(res1);
-                                    _runningTask = null;
-                                }
-                            })
-                            : null;
-                    }
-                });
+                                    { "max-count", count.ToString() },
+                                    { "query", query.Query },
+                                    { "max-dissimilarity", (1.0 - query.SimilarityThreshold).ToString() }
+                                }));
 
-            }
-
-#warning needs to be refactored to separate three parts - cross-threading interaction, loading strategy, querying of target system
-            #region ISearchResult<MadfastResultItem>.ForEachAsync
-            async Task ISearchResult<MadfastResultItem>.ForEachAsync(Func<MadfastResultItem, Task<bool>> body)
-            {   
-                if (_runningTask == null)
-                {
-                    foreach (var item in ReadyResult)
-                    {
-                        var @continue = await body(item);
-                        if (!@continue)
-                        {
-                            return;
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (var itemTask in AsyncResult)
-                    {
-                        var item = await itemTask;
-                        var @continue = item != null && await body(item);
-                        if (!@continue)
-                        {
-                            return;
-                        }
-                    }
-                }
-            }
-
-            public IEnumerable<MadfastResultItem> ReadyResult
-            {
-                get
-                {
-                    lock (_syncObj)
-                    {
-                        if (_runningTask == null)
-                        {
-                            return loaded;
-                        }
-                    }
-                    throw new InvalidOperationException("The result is not ready for synchronous consumption");
-                }
-            }
-
-            Task<MadfastResultItem> TryNext(int index, Task runningTask)
-                => runningTask.ContinueWith(t =>
-                {
-                    lock (_syncObj)
-                    {
-                        if (_runningTask != null && _runningTask.IsFaulted)
-                        {
-                            throw new InvalidOperationException("Loading of results was faulted");
-                        }
-                        return index < loaded.Count
-                            ? loaded[index]
-                            : null;
-                    }
-                });
-
-            public IEnumerable<Task<MadfastResultItem>> AsyncResult
-            {
-                get
-                {
-                    int i = 0; // count of returned elements-1
-                    MadfastResultItem[] ready;
-
-                    do
-                    {
-                        lock (_syncObj)
-                        {
-                            var len = loaded.Count - i;
-                            ready = new MadfastResultItem[len];
-                            loaded.CopyTo(i, ready, 0, len);
-                        }
-
-                        foreach (var item in ready)
-                        {
-                            i++;
-                            yield return Task.FromResult(item);
-                        }
-
-                        Task running = null;
-
-                        lock (_syncObj) // synchronizedCheck to determine if anything new has been loaded
-                        {
-                            if (i == loaded.Count) // if nothing new was loaded, then remember current running task to avoid race
-                            {
-                                running = _runningTask;
+                                var res = r.IsSuccessStatusCode
+                                    ? JsonConvert
+                                        .DeserializeObject<MadfastQueryResult>(await r.Content.ReadAsStringAsync())
+                                        .Targets
+                                        .Select(t => new MadfastResultItem { Ref = t.Targetid, Similarity = 1.0 - t.Dissimilarity })
+                                        .ToList()
+                                    : Enumerable.Empty<MadfastResultItem>();
+                    
+                                updateState(nextFactory?.Invoke(res.Count()), res);
                             }
+
+                            return LoadAndUpdate(fastFetchCount, count => count < _hitLimit ? LoadAndUpdate(_hitLimit, null) : null);
                         }
-
-                        if (running != null) // if running task was remembered in the lock, then no ready elements left and we yield awaiting task
-                        {
-                            yield return TryNext(i, running);
-                            i++;
-                            break; // 
-                        }
-                    } while (true);
-
-                    do
-                    {
-                        Task running;
-                        lock (_syncObj)
-                        {
-                            if (_runningTask == null)
-                            {
-                                break;
-                            }
-                            running = _runningTask;
-                        }
-                        yield return TryNext(i, running);
-                        i++;
-                    } while (true);
-
-                    while (i < loaded.Count)
-                    {
-                        yield return Task.FromResult(loaded[i]);
-                        i++;
-                    }
-                }
-            }
-
-            AsyncOperationStatus IAsyncOperation.Status
-            {
-                get
-                {
-                    if(_runningTask == null)
-                    {
-                        return AsyncOperationStatus.Finished;
-                    }
-                    if(_runningTask.IsFaulted)
-                    {
-                        return AsyncOperationStatus.Faulted;
-                    }
-                    return AsyncOperationStatus.Running;
-                }
-            }
-            #endregion
-
-
-            class MadfastQueryResult
-            {
-                public IEnumerable<Target> Targets { get; set; }
-
-                public class Target
-                {
-                    public double Dissimilarity { get; set; }
-                    public string Targetid { get; set; }
-                }
-            }
-        }
+                    )
+            )
+        );
     }
 }
---
+++ ApiV2/SearchV2.Generics/AsyncResult.cs
@@ -0,0 +1,168 @@
+﻿using SearchV2.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SearchV2.Generics
+{
+    public class AsyncResult<T> : ISearchResult<T>, IAsyncOperation where T : class
+    {
+        volatile Task _runningTask;
+        readonly List<T> loaded = new List<T>();
+        readonly object _syncObj = new object();
+
+        public delegate void UpdateStateDelegate(Task newTask, IEnumerable<T> loadedBatch);
+
+        void UpdateState(Task runningTask, IEnumerable<T> newItems)
+        {
+            lock (_syncObj)
+            {
+                _runningTask = runningTask;
+                loaded.AddRange(newItems);
+            }
+        }
+
+        public AsyncResult(Func<UpdateStateDelegate, Task> loader)
+        {
+            _runningTask = loader(UpdateState);
+        }
+
+        #region ISearchResult<MadfastResultItem>.ForEachAsync
+        async Task ISearchResult<T>.ForEachAsync(Func<T, Task<bool>> body)
+        {
+            if (_runningTask == null)
+            {
+                foreach (var item in ReadyResult)
+                {
+                    var @continue = await body(item);
+                    if (!@continue)
+                    {
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var itemTask in AsyncResultInternal)
+                {
+                    var item = await itemTask;
+                    var @continue = item != null && await body(item);
+                    if (!@continue)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        IEnumerable<T> ReadyResult
+        {
+            get
+            {
+                lock (_syncObj)
+                {
+                    if (_runningTask == null)
+                    {
+                        return loaded;
+                    }
+                }
+                throw new InvalidOperationException("The result is not ready for synchronous consumption");
+            }
+        }
+
+        Task<T> TryNext(int index, Task runningTask)
+            => runningTask.ContinueWith(t =>
+            {
+                lock (_syncObj)
+                {
+                    if (_runningTask != null && _runningTask.IsFaulted)
+                    {
+                        throw new InvalidOperationException("Loading of results was faulted");
+                    }
+                    return index < loaded.Count
+                        ? loaded[index]
+                        : null;
+                }
+            });
+
+        IEnumerable<Task<T>> AsyncResultInternal
+        {
+            get
+            {
+                int i = 0; // count of returned elements-1
+                T[] ready;
+
+                do
+                {
+                    lock (_syncObj)
+                    {
+                        var len = loaded.Count - i;
+                        ready = new T[len];
+                        loaded.CopyTo(i, ready, 0, len);
+                    }
+
+                    foreach (var item in ready)
+                    {
+                        i++;
+                        yield return Task.FromResult(item);
+                    }
+
+                    Task running = null;
+
+                    lock (_syncObj) // synchronizedCheck to determine if anything new has been loaded
+                    {
+                        if (i == loaded.Count) // if nothing new was loaded, then remember current running task to avoid race
+                        {
+                            running = _runningTask;
+                        }
+                    }
+
+                    if (running != null) // if running task was remembered in the lock, then no ready elements left and we yield awaiting task
+                    {
+                        yield return TryNext(i, running);
+                        i++;
+                        break; // 
+                    }
+                } while (true);
+
+                do
+                {
+                    Task running;
+                    lock (_syncObj)
+                    {
+                        if (_runningTask == null)
+                        {
+                            break;
+                        }
+                        running = _runningTask;
+                    }
+                    yield return TryNext(i, running);
+                    i++;
+                } while (true);
+
+                while (i < loaded.Count)
+                {
+                    yield return Task.FromResult(loaded[i]);
+                    i++;
+                }
+            }
+        }
+
+        AsyncOperationStatus IAsyncOperation.Status
+        {
+            get
+            {
+                if (_runningTask == null)
+                {
+                    return AsyncOperationStatus.Finished;
+                }
+                if (_runningTask.IsFaulted)
+                {
+                    return AsyncOperationStatus.Faulted;
+                }
+                return AsyncOperationStatus.Running;
+            }
+        }
+        #endregion
+    }
+}
