fb6e79a102855424b0760baff63d412d8019024d
koskokos <kostya.shnypir@gmail.com>
2018-01-16 11:47:17 +0200

    docker changes

---
+++ ApiV2/SearchV2.Redis/RedisCatalog.cs
@@ -0,0 +1,43 @@
+﻿using SearchV2.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SearchV2.Redis
+{
+    public class RedisCatalog<TId, TFilterQuery, TData> : ICatalogDb<TId, TFilterQuery, TData> 
+        where TData : IWithReference<TId>
+        where TId : IRedisCacheKey
+        where TFilterQuery : IRedisFilterQuery
+    {
+        readonly ICacheClient _cache;
+
+        public RedisCatalog(string connectionString, int database)
+        {
+            
+        }
+
+        Task<IEnumerable<TData>> ICatalogDb<TId, TFilterQuery, TData>.GetAsync(IEnumerable<TId> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<TData>> ICatalogDb<TId, TFilterQuery, TData>.GetFilteredAsync(IEnumerable<TId> ids, TFilterQuery filters)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<TData> ICatalogDb<TId, TFilterQuery, TData>.OneAsync(TId id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IRedisFilterQuery
+    {
+    }
+
+    public interface IRedisCacheKey
+    {
+    }
+}
--- Rdkit.BootstrapDB/run_file.sh
+++ Rdkit.BootstrapDB/run_file.sh
@@ -1,15 +1,14 @@
-psql -h rdkit-postgres -c 'DROP DATABASE IF EXISTS simsearch;'
-psql -h rdkit-postgres -c 'CREATE DATABASE simsearch;'
-psql -h rdkit-postgres simsearch << EndCreate
+psql -h rdkit-postgres -c 'DROP DATABASE IF EXISTS uorsy;'
+psql -h rdkit-postgres -c 'CREATE DATABASE uorsy;'
+psql -h rdkit-postgres uorsy << EndCreate
 CREATE EXTENSION rdkit;
-CREATE TABLE molecules_raw (id SERIAL primary key, smiles text, idnumber text, name text, mw double precision, logp double precision, hba integer, hbd integer, rotb integer, tpsa double precision, fsp3 double precision, hac integer);
+CREATE TABLE mr (id SERIAL primary key, ref text, smiles text);
 EndCreate
-tail -n +2 ./src.txt | psql -h rdkit-postgres -d simsearch -c "COPY molecules_raw (smiles, idnumber, name, mw, logp, hba, hbd, rotb, tpsa, fsp3, hac) FROM STDIN WITH DELIMITER e'\t';"
-psql -h rdkit-postgres simsearch << EOF
-create index raw_name_idx on molecules_raw using hash(name);
-create index raw_idnum_idx on molecules_raw using hash(idnumber);
-select id, mol_from_smiles(smiles::cstring) mol, morganbv_fp(mol_from_smiles(smiles::cstring)) fp into mols from molecules_raw;
-alter table mols add primary key (id);
-alter table mols add foreign key(id) references molecules_raw;
-create index mols_fp_idx on mols using gist(fp);
-create index mols_mol_idx on mols using gist(mol);
+tail -n +2 ./src.txt | psql -h rdkit-postgres -d uorsy -c "COPY mr (smiles, ref) FROM STDIN WITH DELIMITER e'\t';"
+psql -h rdkit-postgres uorsy << EOF
+create index raw_ref_idx on mr using hash(ref);
+select id, mol_from_smiles(smiles::cstring) mol, morganbv_fp(mol_from_smiles(smiles::cstring)) fp into ms from mr;
+alter table ms add primary key (id);
+alter table ms add foreign key (id) references mr;
+create index ms_fp_idx on ms using gist(fp);
+create index ms_mol_idx on ms using gist(mol);
