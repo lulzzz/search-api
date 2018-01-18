0e135c86f739a30e63bdc6a054b263b4ae40d61b
koskokos <kostya.shnypir@gmail.com>
2018-01-18 12:50:29 +0200

    changes in redis.bootstrapper

--- Rdkit.BootstrapDB/run_file.sh
+++ Rdkit.BootstrapDB/run_file.sh
@@ -6,7 +6,6 @@ CREATE TABLE mr (id SERIAL primary key, ref text, smiles text);
 EndCreate
 cat ./src.txt | psql -h rdkit-postgres -d uorsy -c "COPY mr (ref, smiles) FROM STDIN WITH DELIMITER e'\t';"
 psql -h rdkit-postgres uorsy << EOF
-create index raw_ref_idx on mr using hash(ref);
 select id, mol_from_smiles(smiles::cstring) mol, morganbv_fp(mol_from_smiles(smiles::cstring)) fp into ms from mr;
 alter table ms add primary key (id);
 alter table ms add foreign key (id) references mr;
