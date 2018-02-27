80e43444e09ec757bcb2d2461ce68e4b179bd65e
koskokos <kostya.shnypir@gmail.com>
2018-02-27 11:52:55 +0200

    fix in class visibility

--- ApiV2/SearchV2.Api.Uorsy/Program.cs
+++ ApiV2/SearchV2.Api.Uorsy/Program.cs
@@ -17,7 +17,6 @@ namespace SearchV2.Api.Uorsy
     using static ActionDescriptor;
     using static SearchApi;
     using static CompositeSearchService;
-    using System.IO;
 
     class Program
     {
@@ -28,25 +27,13 @@ namespace SearchV2.Api.Uorsy
             public string PostgresConnection { get; set; }
         }
 
-        public class PriceCategory
-        {
-            public class WeightPricePair
-            {
-                public string Weight { get; set; }
-                public string Price { get; set; }
-            }
-            public int Id { get; set; }
-            public string ShippedWithin { get; set; }
-            public IEnumerable<WeightPricePair> WeightsAndPrices { get; set; }
-        }
-
         public static void Main(string[] args)
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
             var env = EnvironmentHelper.Read<Env>();
 
-            var priceCategories = ReadPrices(env.PathToPriceCats).ToArray();
+            var priceCategories = PriceCategory.ReadFromFile(env.PathToPriceCats).ToArray();
 
             var checker = new HashSet<string>();
             var mols = MoleculeData.ReadFromFile(env.PathToCsvSource).Where(md => checker.Add(md.Ref)).ToArray();
@@ -71,25 +58,6 @@ namespace SearchV2.Api.Uorsy
 #warning should be in different controller and should be fitted with molecules as a dictionary according to openapi
                 Get("price-categories", () => priceCategories)
             ).Run();
-        }
-
-        public static IEnumerable<PriceCategory> ReadPrices(string path)
-        {
-            const int commonHeaderLength = 2;
-            using (var reader = new StreamReader(path))
-            {
-                var weights = reader.ReadLine().Split('\t').Skip(commonHeaderLength).ToArray();
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine().Split('\t');
-                    yield return new PriceCategory
-                    {
-                        Id = int.Parse(line[0]),
-                        ShippedWithin = line[1],
-                        WeightsAndPrices = weights.Select((w, i) => new PriceCategory.WeightPricePair { Weight = w, Price = line[i + commonHeaderLength] }).ToArray()
-                    };
-                }
-            }
         }
     }
 }
